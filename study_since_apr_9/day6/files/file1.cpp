978840691a635145afec8aa9612c1ea1f4e0be55
Boyaowang <wangboyao17@outlook.com>
2020-04-14 22:53:00 +0200

    Apr 14 study File_handling ver 1.02: add is_open

--- study_since_apr_9/day6/files/file1.cpp
+++ study_since_apr_9/day6/files/file1.cpp
@@ -5,9 +5,14 @@ using namespace std;
 // we are going to play with files today
 
 int main(int argc, char const *argv[]) {
-  ofstream boyaoFile;
-  boyaoFile.open("Jiechen.txt");
-  boyaoFile << "One of my best friend is Boyao. I am also his best friend forever!!\n";
+  ofstream boyaoFile("Jiechen.txt");
+  //boyaoFile.open("Jiechen.txt");
+  if (boyaoFile.is_open()) {
+    boyaoFile << "One of my best friend is Boyao. I am also his best friend forever!!\n";
+    boyaoFile << "We will trust and believe each other forever!!\n";
+  } else {
+    std::cout << "ERROR!!" << '\n';
+  }
   boyaoFile.close();
   return 0;
 }
