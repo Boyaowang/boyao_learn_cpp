e47b47666883e55988eae176a7b000824297c6f1
Boyaowang <wangboyao17@outlook.com>
2020-04-14 22:40:23 +0200

    Apr 14 study File_handling ver 1.0

---
+++ study_since_apr_9/day6/files/file1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+// we are going to play with files today
+
+int main(int argc, char const *argv[]) {
+  ofstream boyaoFile;
+  boyaoFile.open("Jiechen.txt");
+  boyaoFile << "One of my best friend is Boyao. I am also his best friend forever!!\n";
+  return 0;
+}
