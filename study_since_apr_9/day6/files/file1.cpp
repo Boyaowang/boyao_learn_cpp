a1bb918ff86ae02a8f37647f5e5bb699a85364a7
Boyaowang <wangboyao17@outlook.com>
2020-04-14 22:45:23 +0200

    Apr 14 study File_handling ver 1.01: close file

--- study_since_apr_9/day6/files/file1.cpp
+++ study_since_apr_9/day6/files/file1.cpp
@@ -8,5 +8,6 @@ int main(int argc, char const *argv[]) {
   ofstream boyaoFile;
   boyaoFile.open("Jiechen.txt");
   boyaoFile << "One of my best friend is Boyao. I am also his best friend forever!!\n";
+  boyaoFile.close();
   return 0;
 }
