4950bc2e2233ad1d7a4d2bf7deac0ad803679eec
Boyaowang <wangboyao17@outlook.com>
2020-04-25 14:41:49 +0200

    APR24 study list

---
+++ caveOfProgramming/STL/list/list.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+
+  list<int> numbers;
+
+  numbers.push_back(1);
+  numbers.push_back(2);
+  numbers.push_back(3);
+
+  numbers.push_front(0);
+
+//insert a number to the list
+  list<int>::iterator it = numbers.begin();
+  it++;
+  numbers.insert(it,1999);
+
+  for (list<int>::iterator it = numbers.begin(); it != numbers.end() ; it++) {
+    std::cout << *it << '\n';
+  }
+  std::cout << "****************erase a number from a list****************" << '\n';
+  //erase a number from a list
+  list<int>::iterator iterase = numbers.begin();
+  iterase++;
+  numbers.erase(iterase);
+
+  for (list<int>::iterator it = numbers.begin(); it != numbers.end() ; it++) {
+    std::cout << *it << '\n';
+  }
+
+
+
+  return 0;
+}
---
+++ caveOfProgramming/STL/map/map.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  //Just imagine string could be a name and int could be an age
+  map<string, int> ages;
+
+  ages["jiechen"] = 26;
+  ages["boyao"] = 25;
+  ages["zixin"] = 25;
+  return 0;
+}
